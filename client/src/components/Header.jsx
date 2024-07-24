665b0dede18b5b33cf5f004eaf463b0cc1128fd4
Karan <karannarode999@gmail.com>
2024-07-24 23:59:09 +0530

    profile image functionlity added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const { currentUser } = useSelector(state => state.user)
     return (
         <div className='bg-slate-200'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -13,15 +15,23 @@ function Header() {
                         <li >Home</li>
                     </Link><Link to="/about">
                         <li>About</li>
-                    </Link><Link to="/profile">
-                        <li>Profile</li>
-                    </Link><Link to="/sign-in">
-                        <li><span className='bg-blue-300 py-2.5 px-2.5 rounded-md mx-3'>SignIn</span></li>
+                    </Link>
+                    <Link to="/profile">
+                        {currentUser ? (
+                            <img src={currentUser.profilePicture} alt="profile" className='h-7 w-7 rounded-full object-cover' />
+                        ) :
+
+                            (
+                                <Link to="/sign-in"><li><span className='bg-blue-300 py-2.5 px-2.5 rounded-md mx-3'>SignIn</span></li> </Link>
+                            )}
+
                     </Link>
 
+
+
                 </ul>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
